ad80b07bebd8d020b84575a4e5e508e0eca41147
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2021-11-22 00:11:41 +0530

    Finish week 9 K-Means Clustering

--- lab9/week9New.py
+++ lab9/week9New.py
@@ -68,19 +68,8 @@ class KMeansClustering:
             Cluster assignment of all the samples in the training data
             (M) Vector (M number of samples in the train dataset)(numpy int)
         """
-        def find_distance(a, b):
-            distance_squares = [pow((a[i] - b[i]), 2) for i in range(len(a))]
-            return pow(sum(distance_squares), 0.5)
-
-        result = []
-        for point in data.tolist():
-            distances_from_centroids = []
-            for centroid in self.centroids.tolist():
-                distance = find_distance(point, centroid)
-                distances_from_centroids.append(distance)
-            centroid_number = distances_from_centroids.index(min(distances_from_centroids))
-            result.append(centroid_number)
-        return np.array(result)
+        #TODO
+        pass
 
     def m_step(self, data, cluster_assgn):
         """
@@ -88,49 +77,20 @@ class KMeansClustering:
         Compute the centroids
         Args:
             data: M x D Matrix(M training samples with D attributes each)(numpy float)
+            cluster_assign: Cluster Assignment
         Change self.centroids
         """
-        count_dict = {i:0 for i in cluster_assgn}
-        avg_dict = {key:[] for key, value in count_dict.items()}
-        for idx, point in enumerate(data.tolist()):
-            cluster = cluster_assgn[idx]
-            count_dict[cluster] += 1
-            cluster_total = avg_dict[cluster]
-            if len(cluster_total) == 0:
-                avg_dict[cluster] = point
-            else:
-                avg_dict[cluster] = [cluster_total[i] + point[i] for i in range(len(point))]
-        
-        sorted_cluster_list = list(set(cluster_assgn))
-        sorted_cluster_list.sort()
+        #TODO
+        pass
 
-        centroids = []
-        for cluster in sorted_cluster_list:
-            cluster_count = count_dict[cluster]
-            cluster_sum = avg_dict[cluster]
-            centroids.append([i / cluster_count for i in cluster_sum])
-
-        self.centroids = np.array(centroids)
-
-    def evaluate(self, data):
+    def evaluate(self, data, cluster_assign):
         """
         K-Means Objective
         Args:
             data: Test data (M x D) matrix (numpy float)
+            cluster_assign: M vector, Cluster assignment of all the samples in `data`
         Returns:
             metric : (float.)
         """
-        def find_distance(a, b):
-            distance_squares = [pow((a[i] - b[i]), 2) for i in range(len(a))]
-            return sum(distance_squares)
-
-        # centroid_assigned = self.e_step(data)
-        # result = 0
-        # for i in range(len(data)):
-        #     result += find_distance(data[i], self.centroids[centroid_assigned[i]])
-
-        result = 0
-        for point in data.tolist():
-            for centroid in self.centroids.tolist():
-                result += find_distance(point, centroid)
-        return result
+        #TODO
+        pass
